105485b5e89c845b676d84f440f09e195ad321ad
Travis Gooden <tgooden@my.bcit.ca>
2024-05-15 13:22:06 -0700

    Change to use model_validate, return status code 422 on error

--- backend/analysis-manager/main.py
+++ backend/analysis-manager/main.py
@@ -1,15 +1,25 @@
-from typing import List
-
 import functions_framework
 from flask import Request, make_response, typing
-from pydantic import BaseModel, Field, HttpUrl, ValidationError
+from pydantic import (
+    AliasChoices,
+    BaseModel,
+    Field,
+    HttpUrl,
+    ValidationError,
+)
 
 
 class AnalyzeTextAreaModel(BaseModel):
-    text_body: str = Field(min_length=10, max_length=1000)
-    author_name: List[str] = Field(min_items=1)
+    text_body: str = Field(
+        min_length=10,
+        max_length=5000,
+        validation_alias=AliasChoices("text_body", "text-to-analyze"),
+    )
+    author: str = Field(min_length=1)
     publisher: str = Field(min_length=5)
-    source_url: HttpUrl
+    source_url: HttpUrl = Field(
+        validation_alias=AliasChoices("source_url", "source-url")
+    )
 
 
 @functions_framework.http
@@ -20,20 +30,17 @@ def handler(request: Request) -> typing.ResponseReturnValue:
     :param request: the incoming request
     :return: an empty successful response
     """
+    request_form = {key: request.form[key] for key in request.form}
+    #
     # TODO: validate incoming request
     try:
-        validate_request = AnalyzeTextAreaModel(
-            text_body=str(request.form.get("text-to-analyze")),
-            author_name=str(request.form.get("author")).split(","),
-            publisher=str(request.form.get("publisher")),
-            source_url=str(request.form.get("source-url")),
-        )
+        AnalyzeTextAreaModel.model_validate(request_form)
     except ValidationError as e:
         response = make_response({error["type"]: error["msg"] for error in e.errors()})
+        response.status_code(422)
         return response
 
-    validated_json = validate_request.dict()
-    print(validated_json)
+    request_form["author"] = request_form["author"].split(",")
     # TODO: (maybe) save the document to a storage bucket
     # TODO: send pubsub message to analysis worker(s)
 
