83fd826126cab398f5f71c98f08d10e629de73d3
Alex Krantz <alex@krantz.dev>
2024-05-16 09:30:04 -0700

    Move analysis manager into public component

---
+++ bases/veritasai/analysis_manager/handler.py
@@ -0,0 +1,25 @@
+import functions_framework
+from flask import Request, typing
+from veritasai.input_validation import AnalyzeText, ValidationError, response_from_validation_error
+
+
+@functions_framework.http
+def handler(request: Request) -> typing.ResponseReturnValue:
+    """
+    Initiate the analysis process for a document.
+
+    :param request: the incoming request
+    :return: an empty successful response
+    """
+    try:
+        body = AnalyzeText.model_validate(request.form)
+    except ValidationError as e:
+        return response_from_validation_error(e)
+
+    print(body)
+
+    # TODO: check if the document has already been processed
+    # TODO: (maybe) save the document to a storage bucket
+    # TODO: send pubsub message to analysis worker(s)
+
+    return "", 204
