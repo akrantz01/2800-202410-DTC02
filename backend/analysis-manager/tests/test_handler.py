da5af4068ea2b00b03cdb565c96eab04713a6d9d
Travis Gooden <tgooden@my.bcit.ca>
2024-05-16 10:54:39 -0700

    Change test_handler.py to use flasks native testing

--- backend/analysis-manager/tests/test_handler.py
+++ backend/analysis-manager/tests/test_handler.py
@@ -1,180 +1,211 @@
 import json
+from pathlib import Path
 
-import requests
+import pytest
+from functions_framework import create_app
+
+PROJECT_DIR = Path(__file__).parent.parent
 
 
-def test_all_fields_empty():
-    form_data = {"text-to-analyze": "", "author": "", "publisher": "", "source-url": ""}
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+@pytest.fixture
+def app():
+    """
+    Create a new function application instance.
+    """
+
+    instance = create_app(source=PROJECT_DIR / "main.py", target="handler")
+    instance.config.update({"TESTING": True})
+
+    yield instance
+
+
+@pytest.fixture
+def client(app):
+    """
+    Retrieve the client for the function handler.
+    """
+    return app.test_client()
+
+
+def test_all_fields_empty(client):
+    response = client.post(
+        "/",
+        data={"text-to-analyze": "", "author": "", "publisher": "", "source-url": ""},
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 2 characters",
         "url_parsing": "Input should be a valid URL, input is empty",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_textarea_empty():
-    form_data = {
-        "text-to-analyze": "",
-        "author": "Boris Newsman",
-        "publisher": "Fawks",
-        "source-url": "https://foxnews.com",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
-        "string_too_short": "String should have at least 10 characters",
-    }
-    assert result.status_code == 422
-
-
-def test_author_empty():
-    form_data = {
-        "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
-        "author": "",
-        "publisher": "Fawks",
-        "source-url": "https://foxnews.com",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_author_empty(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
+            "author": "",
+            "publisher": "Fawks",
+            "source-url": "https://foxnews.com",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 5 characters",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_publisher_empty():
-    form_data = {
-        "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
-        "author": "Guy Fawkes",
-        "publisher": "",
-        "source-url": "https://foxnews.com",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_publisher_empty(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
+            "author": "Guy Fawkes",
+            "publisher": "",
+            "source-url": "https://foxnews.com",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 2 characters",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_url_empty():
-    form_data = {
-        "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
-        "author": "Guy Fawkes",
-        "publisher": "Fawkes News",
-        "source-url": "",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_url_empty(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
+            "author": "Guy Fawkes",
+            "publisher": "Fawkes News",
+            "source-url": "",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "url_parsing": "Input should be a valid URL, input is empty",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_all_fields_too_short():
-    form_data = {
-        "text-to-analyze": "g",
-        "author": "j",
-        "publisher": "j",
-        "source-url": "",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_all_fields_too_short(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "g",
+            "author": "j",
+            "publisher": "j",
+            "source-url": "",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 2 characters",
         "url_parsing": "Input should be a valid URL, input is empty",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_textarea_too_short():
-    form_data = {
-        "text-to-analyze": "j",
-        "author": "Boris Newsman",
-        "publisher": "Fawks",
-        "source-url": "https://foxnews.com",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_textarea_too_short(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "j",
+            "author": "Boris Newsman",
+            "publisher": "Fawks",
+            "source-url": "https://foxnews.com",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 10 characters",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_author_too_short():
-    form_data = {
-        "text-to-analyze": "Puppy wins Nobel Prize and immediately eats it.",
-        "author": "Me",
-        "publisher": "Fawks",
-        "source-url": "https://foxnews.com",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_author_too_short(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Puppy wins Nobel Prize and immediately eats it.",
+            "author": "Me",
+            "publisher": "Fawks",
+            "source-url": "https://foxnews.com",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 5 characters",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_publisher_too_short():
-    form_data = {
-        "text-to-analyze": "Puppy wins Nobel Prize and immediately eats it.",
-        "author": "George Strombolopolous",
-        "publisher": "C",
-        "source-url": "https://foxnews.com",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_publisher_too_short(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Puppy wins Nobel Prize and immediately eats it.",
+            "author": "George Strombolopolous",
+            "publisher": "C",
+            "source-url": "https://foxnews.com",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "string_too_short": "String should have at least 2 characters",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_url_invalid_string():
-    form_data = {
-        "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
-        "author": "Guy Fawkes",
-        "publisher": "Fawkes News",
-        "source-url": "Behind ye old windmill",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_url_invalid_string(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
+            "author": "Guy Fawkes",
+            "publisher": "Fawkes News",
+            "source-url": "Behind ye old windmill",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "url_parsing": "Input should be a valid URL, relative URL without a base",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_url_invalid_domain():
-    form_data = {
-        "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
-        "author": "Guy Fawkes",
-        "publisher": "Fawkes News",
-        "source-url": "ftp://Behind ye old windmill",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_url_invalid_domain(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
+            "author": "Guy Fawkes",
+            "publisher": "Fawkes News",
+            "source-url": "ftp://Behind ye old windmill",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "url_parsing": "Input should be a valid URL, invalid domain character",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
 
 
-def test_url_invalid_characters():
-    form_data = {
-        "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
-        "author": "Guy Fawkes",
-        "publisher": "Fawkes News",
-        "source-url": "http://@#%$&%$#%^$#@$",
-    }
-    result = requests.post("http://localhost:8080", data=form_data)
-    result_json = json.loads(result.text)
-    assert result_json == {
+def test_url_invalid_characters(client):
+    response = client.post(
+        "/",
+        data={
+            "text-to-analyze": "Yesterday's news tomorrow, Tonight at 5.",
+            "author": "Guy Fawkes",
+            "publisher": "Fawkes News",
+            "source-url": "http://@#%$&%$#%^$#@$",
+        },
+    )
+    result = json.loads(response.text)
+    assert result == {
         "url_parsing": "Input should be a valid URL, empty host",
     }
-    assert result.status_code == 422
+    assert response.status_code == 422
