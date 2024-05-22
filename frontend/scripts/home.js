0bffa4469cb580d58486914a28d95b642cbc58d1
Alex Krantz <alex@krantz.dev>
2024-05-22 12:04:25 -0700

    Add sending request to analysis manager

---
+++ frontend/scripts/home.js
@@ -0,0 +1,27 @@
+const BACKEND_URL = 'http://localhost:5000';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const data = Object.fromEntries(new FormData(form));
+  for (const key in data) if (data[key].trim().length === 0) delete data[key];
+
+  // TODO: show loading spinner
+
+  const response = await fetch(BACKEND_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+    // TODO: handle errors properly
+    const message = await response.text();
+    console.error(message);
+    return;
+  }
+
+  const result = await response.json();
+  window.location.href = `summary.html?uid=${result.id}`;
+});
