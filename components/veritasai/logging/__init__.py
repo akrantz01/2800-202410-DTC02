5be84bf8d770d8ddf94cde6ccbee7c3b442a12c8
Alex Krantz <alex@krantz.dev>
2024-05-23 16:36:13 -0700

    Integrate logging into analysis manager flow

--- components/veritasai/logging/__init__.py
+++ components/veritasai/logging/__init__.py
@@ -1,4 +1,4 @@
-from logging import getLogger
+from logging import basicConfig, getLogger
 
 from google.cloud import logging
 from veritasai.config import location
@@ -6,6 +6,8 @@ from veritasai.config import location
 if location.is_production:
     client = logging.Client()
     client.setup_logging()
+elif location.is_development:
+    basicConfig(level="INFO")
 
 get_logger = getLogger
 
