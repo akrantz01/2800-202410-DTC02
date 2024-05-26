0363744692e9e79f16816764ccaecd1670d52142
Dylan Reimer <reimer.dylan@gmail.com>
2024-05-25 19:23:01 -0700

    refactor assign article for python

--- development/populate.py
+++ development/populate.py
@@ -11,6 +11,24 @@ def assign_article(article_id: str) -> None:
     if not author_exists(article["author"]):
         if not publisher_exists(article["publisher"]):
             create_publisher(article, article_id)
+        create_author(article, article_id)
+    else:
+        author_id = get_author_id(article["author"])
+        publisher_id = get_publisher_id(article["publisher"])
+        publisher_ref = get_db().collection("publishers").document(publisher_id)
+        publisher_ref.update(
+            {
+                "authors": firestore.ArrayUnion([author_id]),
+                "articles": firestore.ArrayUnion([article_id]),
+            }
+        )
+        author_ref = get_db().collection("authors").document(author_id)
+        author_ref.update(
+            {
+                "publishedFor": firestore.ArrayUnion([publisher_id]),
+                "articles": firestore.ArrayUnion([article_id]),
+            }
+        )
 
 
 def author_exists(author: str) -> bool:
@@ -53,7 +71,7 @@ def publisher_exists(publisher: str) -> bool:
         )
 
 
-def create_publisher(article: dict, article_id) -> None:
+def create_publisher(article: dict, article_id: str) -> None:
     publisher = {
         "aiScore": article["aiScore"],
         "biasScore": article["biasScore"],
@@ -64,17 +82,36 @@ def create_publisher(article: dict, article_id) -> None:
     get_db().collection("publishers").add(publisher)
 
 
+def get_publisher_id(publisher: str) -> str:
+    publishers_ref = get_db().collection("publishers").stream()
+    for publishers in publishers_ref:
+        if (
+            publishers.to_dict()["name"].replace(" ", "").lower()
+            == publisher.replace(" ", "").lower()
+        ):
+            return publishers.id
+
+
+def get_author_id(author: str) -> str:
+    authors_ref = get_db().collection("authors").stream()
+    for authors in authors_ref:
+        if authors.to_dict()["name"].replace(" ", "").lower() == author.replace(" ", "").lower():
+            return authors.id
+
+
 def main():
-    article = {
-        "aiScore": 0.69,
-        "biasScore": 0.420,
-        "publisher": "Questionable News",
-        "author": "John Smith",
-    }
+    # article = {
+    #     "aiScore": 0.69,
+    #     "biasScore": 0.420,
+    #     "publisher": "Questionable News",
+    #     "author": "John Smith",
+    # }
     # print(author_exists("Johnsmith   "))
     # print(publisher_exists("Old Fashioned News"))
     # create_publisher(article, "demo")
-    create_author(article, "demo")
+    # create_author(article, "demo")
+    print(get_publisher_id("Questionable News"))
+    assign_article("demo")
 
 
 if __name__ == "__main__":
