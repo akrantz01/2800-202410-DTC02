9ed34a03ddf3d9aa1064fe96f2c05963f3a94d5e
Dylan Reimer <reimer.dylan@gmail.com>
2024-05-25 19:30:34 -0700

    Include docstrings

--- development/populate.py
+++ development/populate.py
@@ -3,6 +3,16 @@ from veritasai.firebase import get_db
 
 
 def assign_article(article_id: str) -> None:
+    """
+    Assign an article to an author and publisher.
+
+    If an author doesn't exist, an author will be created. If a publisher doesn't exist, a publisher
+    will be created. Publishers are always created before authors, so an author will not exist
+    without a publisher.
+
+    :param article_id: a string for the article id of the article that was just created
+    :raises KeyError: if the article does not exist in the DB
+    """
     article_ref = get_db().collection("articles").document(article_id)
     article_doc = article_ref.get()
     if not article_doc:
@@ -16,6 +26,7 @@ def assign_article(article_id: str) -> None:
         author_id = get_author_id(article["author"])
         publisher_id = get_publisher_id(article["publisher"])
         publisher_ref = get_db().collection("publishers").document(publisher_id)
+        # update ai/bias score
         publisher_ref.update(
             {
                 "authors": firestore.ArrayUnion([author_id]),
@@ -23,6 +34,7 @@ def assign_article(article_id: str) -> None:
             }
         )
         author_ref = get_db().collection("authors").document(author_id)
+        # update ai/bias score
         author_ref.update(
             {
                 "publishedFor": firestore.ArrayUnion([publisher_id]),
@@ -32,6 +44,11 @@ def assign_article(article_id: str) -> None:
 
 
 def author_exists(author: str) -> bool:
+    """
+    Check if the author exists in the DB.
+
+    :param author: The author name
+    """
     authors_ref = get_db().collection("authors").stream()
     for authors in authors_ref:
         return (
@@ -42,6 +59,12 @@ def author_exists(author: str) -> bool:
 
 
 def create_author(article: dict, article_id: str) -> None:
+    """
+    Create an author in the DB.
+
+    :param article: a dictionary representing the article from the DB
+    :param article_id: the article id
+    """
     publishers_ref = get_db().collection("publishers").stream()
     for publisher in publishers_ref:
         if (
@@ -61,6 +84,11 @@ def create_author(article: dict, article_id: str) -> None:
 
 
 def publisher_exists(publisher: str) -> bool:
+    """
+    Check if the publisher exists in the DB.
+
+    :param publisher: The publisher name
+    """
     publishers_ref = get_db().collection("publishers").stream()
     for publishers in publishers_ref:
         return (
@@ -72,6 +100,12 @@ def publisher_exists(publisher: str) -> bool:
 
 
 def create_publisher(article: dict, article_id: str) -> None:
+    """
+    Create a publisher in the DB.
+
+    :param article: a dictionary representing the article from the DB
+    :param article_id: the article id
+    """
     publisher = {
         "aiScore": article["aiScore"],
         "biasScore": article["biasScore"],
@@ -83,6 +117,12 @@ def create_publisher(article: dict, article_id: str) -> None:
 
 
 def get_publisher_id(publisher: str) -> str:
+    """
+    Get the publisher ID.
+
+    :param publisher: the publisher name
+    :return: publisher ID as a string
+    """
     publishers_ref = get_db().collection("publishers").stream()
     for publishers in publishers_ref:
         if (
@@ -93,6 +133,12 @@ def get_publisher_id(publisher: str) -> str:
 
 
 def get_author_id(author: str) -> str:
+    """
+    Get the author ID.
+
+    :param author: the author name
+    :return: author ID as a string
+    """
     authors_ref = get_db().collection("authors").stream()
     for authors in authors_ref:
         if authors.to_dict()["name"].replace(" ", "").lower() == author.replace(" ", "").lower():
@@ -100,6 +146,9 @@ def get_author_id(author: str) -> str:
 
 
 def main():
+    """
+    Drive the program.
+    """
     # article = {
     #     "aiScore": 0.69,
     #     "biasScore": 0.420,
