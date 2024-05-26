1a30327d6925ed6c8a9b5e52d3bf65326125ffee
Dylan Reimer <reimer.dylan@gmail.com>
2024-05-25 18:20:38 -0700

    Check if publisher is in the DB already

--- development/populate.py
+++ development/populate.py
@@ -1,9 +1,12 @@
 from veritasai.firebase import get_db
 
-# def assign_article(article_id: str) -> None:
-#     article_ref = get_db().collection("articles").document(article_id)
-#     authors_ref = get_db().collection("authors")
-#     publishers_ref = get_db().collection("publishers")
+
+def assign_article(article_id: str) -> None:
+    article_ref = get_db().collection("articles").document(article_id)
+    article_doc = article_ref.get()
+    if not article_doc:
+        raise KeyError("Article ID does not exist")
+    # article = article_doc.to_dict()
 
 
 def author_exists(author: str) -> bool:
@@ -16,8 +19,20 @@ def author_exists(author: str) -> bool:
         )
 
 
+def publisher_exists(publisher: str) -> bool:
+    publishers_ref = get_db().collection("publishers").stream()
+    for publishers in publishers_ref:
+        return (
+            True
+            if publishers.to_dict()["name"].replace(" ", "").lower()
+            == publisher.replace(" ", "").lower()
+            else False
+        )
+
+
 def main():
     print(author_exists("Johnsmith   "))
+    print(publisher_exists("Old Fashioned News"))
 
 
 if __name__ == "__main__":
