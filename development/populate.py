8075fe5d445ffa083ab952a9d3b984aeccb9781d
Dylan Reimer <reimer.dylan@gmail.com>
2024-05-27 10:45:22 -0700

    Update the publisher ai score

--- development/populate.py
+++ development/populate.py
@@ -221,6 +221,28 @@ def update_publisher_bias(publisher_id: str = "", publisher_name: str = "") -> N
     publisher_ref.update({"biasScore": total_score / total_count})
 
 
+def update_publisher_ai(publisher_id: str = "", publisher_name: str = "") -> None:
+    """
+    Update the publisher ai score in firestore.
+
+    :param publisher_id" the firestore id for the publisher - optional
+    :param publisher_name: the name of the publisher if id is not available
+                          - optional - overrides id
+    """
+    if publisher_name:
+        publisher_id = get_publisher_id(publisher_name)
+    publisher_ref = get_db().collection("publishers").document(publisher_id)
+    publisher = publisher_ref.get().to_dict()
+    total_score = 0
+    total_count = 0
+    for article in publisher["articles"]:
+        total_score += float(
+            get_db().collection("articles").document(article).get().to_dict()["aiScore"]
+        )
+        total_count += 1
+    publisher_ref.update({"aiScore": total_score / total_count})
+
+
 def main():
     """
     Drive the program.
@@ -238,7 +260,7 @@ def main():
     # print(get_publisher_id("Questionable News"))
     # assign_article("demo")
     # add_history("demo", "K8n5TZsfPogedpftAREoQVhJ7Dc2")
-    update_publisher_bias(publisher_name="Fun Time News")
+    update_publisher_ai(publisher_name="Fun Time News")
 
 
 if __name__ == "__main__":
