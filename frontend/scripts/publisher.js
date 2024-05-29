d3b48790c651f0b9035c59d472fdab9ec4d34803
Dylan Reimer <reimer.dylan@gmail.com>
2024-05-29 12:53:45 -0700

    Update publisher history from firestore

--- frontend/scripts/publisher.js
+++ frontend/scripts/publisher.js
@@ -57,32 +57,51 @@ async function writePublisherArticles() {
   publisherArticles.forEach(async (article) => {
     const articleID = article;
     let articleBody;
+    let aiGauge;
+    let biasGauge;
+    let articleExists = false;
 
     const articleSnapshot = await getDoc(doc(firestore, 'articles', articleID));
     if (articleSnapshot.exists()) {
       articleBody = articleSnapshot.data().scannedText;
+      articleExists = true;
+      if (articleSnapshot.data().ai) aiGauge = `width: ${articleSnapshot.data().ai.aiScore * 100}%`;
+      if (articleSnapshot.data().bias)
+        biasGauge = `width: ${articleSnapshot.data().bias.biasScore * 100}%`;
+
       // get other data from article
     } else {
       console.log('article missing from firestore');
-      articleBody = 'Article Missing.';
+      articleBody = 'Article Reference Missing';
+      aiGauge = 'width: 0%';
+      biasGauge = 'width: 0%';
       // Assign defaults if article is missing
     }
     const myTemplate = document.getElementById('saved-card');
 
     const newCard = myTemplate.content.cloneNode(true);
     newCard.querySelector('.analyzed-text').innerHTML = articleBody;
-    newCard.querySelector('.ai-gauge').style = 'width: 2%';
-    newCard.querySelector('.bias-gauge').style = 'width: 80%';
-    newCard.querySelector('.link').addEventListener('click', () => {
-      window.location.href = 'summary?uid=' + articleID;
-    });
-    const buttonID = 'save-' + articleID;
-    newCard.querySelector('.save-button').id = buttonID;
-    const buttonElement = newCard.getElementById(buttonID);
-    if (savedArticles.includes(article)) buttonElement.classList.add('fill-primary');
-    buttonElement.addEventListener('click', () => {
-      saveArticleToggle(articleID);
-    });
+    if (aiGauge) newCard.querySelector('.ai-gauge').style = aiGauge;
+    else newCard.querySelector('.ai-tag').innerHTML = 'No AI score available';
+
+    if (biasGauge) newCard.querySelector('.bias-gauge').style = biasGauge;
+    else newCard.querySelector('.bias-tag').innerHTML = 'No Bias score available';
+    if (articleExists) {
+      newCard.querySelector('.link').addEventListener('click', () => {
+        window.location.href = 'summary?uid=' + articleID;
+      });
+      const buttonID = 'save-' + articleID;
+      newCard.querySelector('.save-button').id = buttonID;
+      const buttonElement = newCard.getElementById(buttonID);
+      if (savedArticles.includes(article)) buttonElement.classList.add('fill-primary');
+      buttonElement.addEventListener('click', () => {
+        saveArticleToggle(articleID);
+      });
+    } else {
+      newCard.querySelector('.link').classList.add('hidden');
+      newCard.querySelector('.save-button').classList.add('hidden');
+      newCard.querySelector('.gauges').classList.add('hidden');
+    }
     document.getElementById('publisher-cards').appendChild(newCard);
   });
 }
