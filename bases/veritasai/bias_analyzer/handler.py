7e9f315500ea99f851cda672695057c58cd3cf6e
Alex Krantz <alex@krantz.dev>
2024-05-30 15:00:52 -0700

    Progressively populate sentence analysis from bias analyzer

--- bases/veritasai/bias_analyzer/handler.py
+++ bases/veritasai/bias_analyzer/handler.py
@@ -1,7 +1,7 @@
 import functions_framework
 from cloudevents.http import CloudEvent
 from veritasai.articles import Article
-from veritasai.bias import analyze
+from veritasai.bias import analyze_document, analyze_sentence, get_keyword_sentences
 from veritasai.firebase import get_db
 from veritasai.logging import get_logger
 
@@ -18,13 +18,27 @@ def handler(event: CloudEvent):
     article = Article.from_cloud_event(event)
     logger.info("received article %(id)s", {"id": article.id})
 
-    bias = analyze(text=article.content)
+    db = get_db()
+    document = db.collection("articles").document(article.id)
 
-    logger.info("analysis complete, writing to database")
+    bias = analyze_document(text=article.content)
 
-    get_db().collection("articles").document(article.id).update(
+    logger.info("document analysis complete, writing to database")
+    document.update(
         {
             "bias": bias,
             "status.bias": "complete",
         }
     )
+
+    logger.info("starting sentence analysis")
+
+    for keyword, sentences in get_keyword_sentences(bias):
+        for i, sentence in enumerate(sentences):
+            logger.info("processing sentence %d for keyword %s", i, keyword)
+
+            analyze_sentence(sentence)
+            document.update({db.field_path("bias", "keywords", keyword, "sentences"): sentences})
+
+    logger.info("sentence analysis complete, writing to database")
+    document.update({"status.sentences": "complete"})
